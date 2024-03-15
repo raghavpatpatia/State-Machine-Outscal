6c4209479b8bf603d84e810b328c6300a3c5f56c
Raghav Patpatia <raghav.patpatia@gmail.com>
2024-03-15 20:55:09 +0530

    State Machine
    Added PatrolManStateMachine and PatrolManController

---
+++ Assets/Scripts/Enemy/PatrolMan/PatrolManController.cs
@@ -0,0 +1,34 @@
+﻿using StatePattern.Player;
+
+namespace StatePattern.Enemy
+{
+    public class PatrolManController : EnemyController
+    {
+        private PatrolManStateMachine stateMachine;
+
+        public PatrolManController(EnemyScriptableObject enemyScriptableObject) : base(enemyScriptableObject)
+        {
+            enemyView.SetController(this);
+            CreateStateMachine();
+            stateMachine.ChangeState(StateMachine.States.IDLE);
+        }
+
+        private void CreateStateMachine() => stateMachine = new PatrolManStateMachine(this);
+
+        public override void UpdateEnemy()
+        {
+            if (currentState == EnemyState.DEACTIVE)
+                return;
+
+            stateMachine.Update();
+        }
+
+        public override void PlayerEnteredRange(PlayerController targetToSet)
+        {
+            base.PlayerEnteredRange(targetToSet);
+            stateMachine.ChangeState(StateMachine.States.CHASING);
+        }
+
+        public override void PlayerExitedRange() => stateMachine.ChangeState(StateMachine.States.IDLE);
+    }
+}
